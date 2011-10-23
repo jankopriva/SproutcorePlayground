03dd44222870d3bc60175dbc4c9f6fe81ffb0ab7
Jan Kopriva <jan.kopriva@gooddata.com>
2011-10-23 20:50:41 +0200

    Initial commit

---
+++ starter-kit/js/app.js
@@ -0,0 +1,28 @@
+var Todos = SC.Application.create();
+
+Todos.Todo = SC.Object.extend({
+	title: null,
+	isDone: false
+});
+
+Todos.todosController = SC.ArrayProxy.create({
+	content: [],
+
+	createTodo: function(title) {
+		var todo = Todos.Todo.create({title: title});
+		this.pushObject(todo);
+	}
+});
+
+Todos.CreateTodoView = SC.TextField.extend({
+	insertNewline: function() {
+		var value = this.get('value');
+
+		if (value) {
+			Todos.todosController.createTodo(value);
+			this.set('value', '');
+		}
+	}
+});
+
+
